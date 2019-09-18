089c84539e6bccdbf58a66f149d0ad3b53ae3ae3
Jeremy Ward <celloward@gmail.com>
2019-09-18 10:27:24 -0500

    add three_in_a_row mehtod

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,15 @@ class TicTacToe
   end
 
 private
+  def three_in_a_row?
+    for a in (0..3)
+      x = 0
+      o = 0
+      for b in (0..3)
+        if yield 
+
   def game_over?
+    three_in_a_row?({ |a, b| ttt_board[b][a] })
   end
 
 end
