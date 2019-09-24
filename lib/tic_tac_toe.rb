3c10d664eb4225055bfbac7c37a2e3dd25d6aedf
Jeremy Ward <celloward@gmail.com>
2019-09-24 11:40:12 -0500

    Refactoring after Tim

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,29 +1,10 @@
-require "matrix"
-
-class Array
-  def in_a_row? match, number
-    for row_index in (0..self.count - 1)
-      counter = 0
-      for column_index in (0..self[row_index].count - 1)
-        if block_given?
-          yield(row_index, column_index) == match ? counter += 1 : counter = 0
-          return true if counter >= number
-        end
-      end
-    end
-    false
-  end
-end
-
 class TicTacToe
   attr_accessor :ttt_board, :x_turn
   attr_reader :player
   def initialize
-    @ttt_board = [[1, 2, 3],
-                  [4, 5, 6],
-                  [7, 8, 9]]
-    @x_turn = 1
-    @player = ["O", "X"]
+    @ttt_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @x_turn = true
+    @player = :X
   end
 
   def play!
@@ -37,13 +18,13 @@ class TicTacToe
     @board =<<~BOARD
 
          |     |
-      #{ttt_board[0][0]}  |  #{ttt_board[0][1]}  |  #{ttt_board[0][2]}
+      #{ttt_board[0]}  |  #{ttt_board[1]}  |  #{ttt_board[2]}
     -----|-----|-----
          |     |     
-      #{ttt_board[1][0]}  |  #{ttt_board[1][1]}  |  #{ttt_board[1][2]}  
+      #{ttt_board[3]}  |  #{ttt_board[4]}  |  #{ttt_board[5]}  
     -----|-----|-----
          |     |     
-      #{ttt_board[2][0]}  |  #{ttt_board[2][1]}  |  #{ttt_board[2][2]}  
+      #{ttt_board[6]}  |  #{ttt_board[7]}  |  #{ttt_board[8]}  
          |     |     
 
     BOARD
@@ -52,47 +33,46 @@ class TicTacToe
 
   def turn
     while !self.game_over?
-      print "#{player[x_turn]}: "
+      x_turn == true ? self.player = :X : self.player = :O
+      print "#{player}: "
       change(gets.chomp.to_i)
       puts `clear`
       puts board
+      self.x_turn = !x_turn
     end
+    puts game_over?
   end
 
   def change number
-      if (1..9).include?(number.to_i) && ttt_board.flatten.include?(number.to_i)
-        index = Matrix[*ttt_board].index(number)
-        if x_turn == 1
-          self.ttt_board[index[0]][index[1]] = :X
-          self.x_turn = 0
-        else
-          self.ttt_board[index[0]][index[1]] = :O
-          self.x_turn = 1
-        end
+      if (1..9).include?(number) && ttt_board.include?(number)
+        index = ttt_board.index(number)
+          self.ttt_board[index] = player
       else
         puts "Not a valid entry. Please try again."
         change(gets.chomp.to_i)
       end
   end
 
-  def pan_directional match
-    ttt_board.in_a_row?(match, 3) { |row, item| ttt_board[row][item] } ||
-      ttt_board.in_a_row?(match, 3) { |row, item| ttt_board[item][row] } ||
-      ttt_board.in_a_row?(match, 3) { |row, item| ttt_board[item][item] } ||
-      ttt_board.in_a_row?(match, 3) { |row, item| ttt_board[item][2 - item] }
+  def seqential? board, match, outer_step, inner_step
+    board.step(outer_step).each do |index|
+      return true if index.select { |i| board[i-1] }.all?(match) 
+      index += inner_step
+    end
+  end
+
+  def tic_tac_toe match
+    sequential?(ttt_board, match, 3, 1) { |row, item| ttt_board[row][item] } ||
+      sequential?(ttt_board, match, 1, 3) { |row, item| ttt_board[item][row] } ||
+      sequential?(ttt_board, match, 4, 0) { |row, item| ttt_board[item][item] } ||
+      match == board[2] == board[4] == board[6]
   end
 
   def game_over?
-    if pan_directional(:X)
-      puts "X wins!"
-    elsif pan_directional(:O)
-      puts "O wins!"
-    elsif ttt_board.flatten.all?(Symbol)
-      puts "Draw!"
-    else
-      return false
+    if tic_tac_toe(player)
+      player
+    elsif ttt_board.all?(Symbol)
+      "Draw!"
     end
-    true
   end
 
 end
