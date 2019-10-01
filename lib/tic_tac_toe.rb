a252c572d78095cfc65c055bbdd831b545d19f05
Cello Ward <celloward@gmail.com>
2019-10-01 10:44:14 -0500

    Cleaned up original code

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,4 @@
 class TicTacToe
-# attr_accessor :x_turn, :player, :ttt_board
 
   def initialize
     @ttt_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -36,7 +35,7 @@ class TicTacToe
   end
 
   def turn
-    while !game_over?
+    while !game_over
       x_turn == true ? self.player = :X : self.player = :O
       print "#{player}: "
       change(gets.chomp.to_i)
@@ -44,34 +43,33 @@ class TicTacToe
       puts board
       self.x_turn = !x_turn
     end
-    puts game_over?
+    puts game_over
   end
 
   def change number
       if (1..9).include?(number) && ttt_board.include?(number)
-        index = ttt_board.index(number)
-          ttt_board[index] = player
+          ttt_board[number - 1] = player
       else
         puts "Not a valid entry. Please try again."
         change(gets.chomp.to_i)
       end
   end
 
-  def three_sequential? matrix, match, outer_step, inner_step
+  def three_sequential_row_column? board, match, outer_step, inner_step
     [0, outer_step, outer_step * 2].each do |index|
-      return true if [index, index + inner_step, index + inner_step * 2].map { |i| matrix[i] }.all?(match)
+      return true if [board[index], board[index + inner_step], board[index + inner_step * 2]].all?(match)
     end
     false
   end
  
   def tic_tac_toe match
-    three_sequential?(ttt_board, match, 3, 1) ||
-      three_sequential?(ttt_board, match, 1, 3) ||
+    three_sequential_row_column?(ttt_board, match, 3, 1) ||
+      three_sequential_row_column?(ttt_board, match, 1, 3) ||
        [ttt_board[0], ttt_board[4], ttt_board[8]].all?(match) ||
       [ttt_board[2], ttt_board[4], ttt_board[6]].all?(match)
   end
 
-  def game_over?
+  def game_over
     if tic_tac_toe(player)
       "#{player} wins!"
     elsif ttt_board.all?(Symbol)
