370cec9004581df956d9a9e4877ea9253d3ea51a
Cello Ward <celloward@gmail.com>
2019-10-17 16:48:58 -0500

    Update README and spec

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,57 +1,45 @@
 require "./lib/tic_tac_toe"
 
 describe TicTacToe do
-    it "doesn't allow multiple plays on same space"
-    
-#    it "only accepts legal numerical input" do
-#        game = TicTacToe.new
-#        game.change("abc").should_receive(:Not a valid entry. Please try again.)
-#    end
 
     it "changes the board when player enters valid play" do
-        subject.change(3)
-        expect(subject.ttt_board).to eql([1, 2, :X, 4, 5, 6, 7, 8, 9])
+        subject.send(:change, 3)
+        expect(subject.send(:ttt_board)).to eql([1, 2, :X, 4, 5, 6, 7, 8, 9])
     end
 
     it "displays graphical board" do
-        expect(subject.board).to eql("\n     |     |     \n  1  |  2  |  3  \n-----|-----|-----\n     |     |     \n  4  |  5  |  6  \n-----|-----|-----\n     |     |     \n  7  |  8  |  9  \n     |     |     \n\n")
+        expect(subject.send(:board)).to eql("\n     |     |     \n  1  |  2  |  3  \n-----|-----|-----\n     |     |     \n  4  |  5  |  6  \n-----|-----|-----\n     |     |     \n  7  |  8  |  9  \n     |     |     \n\n")
     end
 
     it "displays changes on graphical board" do
-        subject.ttt_board = [:O, :X, :O, :X, :X, 6, :X, :O, :X]
-        expect(subject.board).to eql("\n     |     |     \n  O  |  X  |  O  \n-----|-----|-----\n     |     |     \n  X  |  X  |  6  \n-----|-----|-----\n     |     |     \n  X  |  O  |  X  \n     |     |     \n\n")
+        subject.send(:ttt_board=, [:O, :X, :O, :X, :X, 6, :X, :O, :X])
+        expect(subject.send(:board)).to eql("\n     |     |     \n  O  |  X  |  O  \n-----|-----|-----\n     |     |     \n  X  |  X  |  6  \n-----|-----|-----\n     |     |     \n  X  |  O  |  X  \n     |     |     \n\n")
     end
 
     it "ends when all of one symbol occupy three horizontal spaces in a row" do
-        subject.ttt_board = [:X, :X, :X, 4, 5, 6, 7, 8, 9]
-        expect(subject.game_over(ttt_board)).to eql("X wins!")
+        subject.send(:ttt_board=, [:X, :X, :X, 4, 5, 6, 7, 8, 9])
+        expect(subject.send(:game_over)).to eql(:X)
     end
 
     it "ends when all of one symbol occupy three vertical spaces in a row" do
-        subject.ttt_board = [1, 2, :O, 4, 5, :O, 7, 8, :O]
-        subject.player = :O
-        expect(subject.game_over(ttt_board)).to eql("O wins!")
+        subject.send(:ttt_board=, [1, 2, :O, 4, 5, :O, 7, 8, :O])
+        subject.send(:player=, :O)
+        expect(subject.send(:game_over)).to eql(:O)
     end
 
     it "ends when all of one symbol occupy three diagonal spaces in a row" do
-        subject.ttt_board = [1, 2, :X, 4, :X, 5, :X, 8, 9]
-        expect(subject.game_over(ttt_board)).to eql("X wins!")
+        subject.send(:ttt_board=, [1, 2, :X, 4, :X, 5, :X, 8, 9])
+        expect(subject.send(:game_over)).to eql(:X)
     end
 
     it "ends when all spaces are filled and there are no three in a row" do
-        subject.ttt_board = [:O, :X, :O, :X, :X, :O, :X, :O, :X]
-        expect(subject.game_over(ttt_board)).to eql(false)
+        subject.send(:ttt_board=, [:O, :X, :O, :X, :X, :O, :X, :O, :X])
+        expect(subject.send(:game_over)).to eql(true)
     end
 
     it "doesn't end until winner or draw" do
-        subject.ttt_board = [:O, :X, :O, :X, :X, 6, :X, :O, :X]
-        expect(subject.game_over(ttt_board)).to eql(nil)
-    end
-
-    describe "Array#three_sequential?" do
-
-      it "does various righteous things"
-
+        subject.send(:ttt_board=, [:O, :X, :O, :X, :X, 6, :X, :O, :X])
+        expect(subject.send(:game_over)).to eql(false)
     end
 
 end
